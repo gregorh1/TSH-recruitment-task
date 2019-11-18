ccbae185b901193be32691c82cbdf172dc1c82d5
Grzegorz <grzegorz.hul@gmail.com>
2019-11-18 16:33:41 +0100

    task-commit[3.2] fetch history from api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
-
+import moment from 'moment';
 
 export class App {
   initializeApp() {
@@ -10,11 +10,16 @@ export class App {
       const userName = $('.username.input').val();
 
       fetch('https://api.github.com/users/' + userName)
-        .then(response => response.json())
+        .then(response => {
+          if (!response.ok) throw new Error(response.status)
+          else return response.json()
+        })
         .then((respJson) => {
           self.profile = respJson;
           self.updateProfile();
+          self.updateHistory(respJson.login);
         })
+        .catch(err => console.error(err))
     })
 
     $('.username.input').on('keyup', self.validation)
@@ -39,5 +44,54 @@ export class App {
       input.addClass('not-valid')
       btn.style.pointerEvents = "none";
     }
+  }
+
+  updateHistory(username) {
+    const url = `https://api.github.com/users/${username}/events/public`
+    fetch(url)
+      .then(response => {
+        if (!response.ok) throw new Error(response.status)
+        else return response.json()
+      })
+      .then(respJson => {
+        const eventTypes = [
+          'PullRequestEvent',
+          'PullRequestReviewCommentEvent'
+        ]
+        const events = respJson.filter(event => {
+          if (eventTypes.indexOf(event.type) !== -1) {
+            return event
+          }
+        })
+
+        let timelineElements = ''
+        events.forEach((event, i) => {
+          let comment = ''
+          if (event.payload.comment) {
+            comment = ` <a href="${event.payload.comment.html_url}">comment</a> to`
+          }
+          timelineElements +=
+            `<div class="timeline-item ${i === 1 ? "is-primary" : ''}">
+              <div class="timeline-marker ${i === 1 ? "is-primary" : ''}"></div>
+              <div class="timeline-content">
+                <p class="heading">${moment(event.created_at).format("MMM DD, YYYY")}</p>
+                <div class="content">
+                  <img src="${event.actor.avatar_url}">
+                  <span>
+                    <a href="${event.actor.url}">${event.actor.login}</a>
+                    ${event.payload.action + comment}
+                    <a href="${event.payload.pull_request.html_url}">pull request</a>
+                    <p class="repo-name">
+                      <a href="${event.repo.url}">${event.repo.name}</a>
+                    </p>
+                  </span>
+                </div>
+              </div>
+            </div>`
+        });
+
+        $('#user-timeline').html(timelineElements)
+      })
+      .catch(err => console.error(err))
   }
 }
