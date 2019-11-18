d96761c33bd62b3d96c7c6beda1865ffd2678a07
Grzegorz <grzegorz.hul@gmail.com>
2019-11-18 12:29:56 +0100

    task-commit[3.1] validation

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ export class App {
           self.updateProfile();
         })
     })
+
+    $('.username.input').on('keyup', self.validation)
   }
 
   updateProfile() {
@@ -23,5 +25,19 @@ export class App {
     $('#profile-image').attr('src', this.profile.avatar_url)
     $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
     $('#profile-bio').text(this.profile.bio || '(no information)')
+  }
+
+  validation() {
+    const input = $('input.username');
+    const btn = document.querySelector('.load-username')
+    const pattern = /^([a-z0-9-_]+)$/;
+    if (pattern.test(input.val())) {
+      input.removeClass('not-valid')
+      btn.style.pointerEvents = "auto";
+    }
+    else {
+      input.addClass('not-valid')
+      btn.style.pointerEvents = "none";
+    }
   }
 }
